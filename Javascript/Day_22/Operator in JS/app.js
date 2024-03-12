f44156f82bc13921e21370bbdb36b72eefd2cda8
Aditya Tile <adityatile99@gmail.com>
2024-03-12 21:22:09 +0530

    Complete day 22

--- Javascript/Day_22/Operator in JS/app.js
+++ Javascript/Day_22/Operator in JS/app.js
@@ -41,3 +41,42 @@ console.log((m -= n));
 console.log((m *= n));
 console.log((m /= n));
 console.log((m %= n));
+
+// Comparison Operators:
+// Comparision operators to compare to values.
+// 1.>
+// 2.>=
+// 3.<
+// 4.<=
+// 5.==
+// 6.!=
+
+// 1.> (Greater than)
+
+let my_Age = 18;
+console.log(myAge > 18);
+
+//2.>= (Greater than or equal to)
+
+let num = 10;
+console.log(num >= 5);
+
+//3.< (Less than)
+
+let num1 = 40;
+console.log(num1 < 20);
+
+//4.<= (Less than or equal to)
+
+let num2 = 30;
+console.log(num2 <= 40);
+
+// 5.== (equal to equal to)
+
+let numA = 30;
+let numB = 30;
+console.log(numA == numB);
+
+// 6.!= (Not Equal to)
+
+console.log(numA != numB);
