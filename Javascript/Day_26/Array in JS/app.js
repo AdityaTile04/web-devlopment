cf5faf3319f46e73434e3d43bf365e4f293c0229
Aditya Tile <adityatile99@gmail.com>
2024-03-16 21:42:07 +0530

    Started learning Arrays

---
+++ Javascript/Day_26/Array in JS/app.js
@@ -0,0 +1,23 @@
+//! Array (Data Structure)
+// Linear collection of things
+
+// Example of Array
+let students = ["Aditya", "Kunal", "Rushikesh", "Gaurav", "Ujjwal"];
+
+//! Creating Arrays
+let stds = ["Aditya", "Kunal", "Rushikesh", "Gaurav", "Ujjwal"];
+
+let nums = [2, 4, 6, 8, 10];
+
+let marks = [70, 60, 82, 90, 100];
+
+let info = ["Aditya", 19, 8.5]; // mixed array
+
+let newArr = [];
+
+//! Arrays are mutable
+
+let fruits = ["Apple", "Banana", "Orange"];
+fruits[0] = "Mango";
+fruits[1] = "Pineapple"
+console.log(fruits);
