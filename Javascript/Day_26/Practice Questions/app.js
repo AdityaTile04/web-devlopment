244702370a65e24d11bc26308d3578aa3a28d225
Aditya Tile <adityatile99@gmail.com>
2024-03-16 20:22:32 +0530

    in day 26 i learned various string methods and solved practice questions

---
+++ Javascript/Day_26/Practice Questions/app.js
@@ -0,0 +1,17 @@
+//! Practice Queston 1
+
+let msg = "help!";
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
+
+//! Practice Queston 2
+
+let str = "ApnaCollege";
+console.log(str.slice(4, 9));
+console.log(str.indexOf("na"));
+console.log(str.replace("Apna", "Our"));
+
+//! Practice Queston 3
+
+let newStr = str.slice(4).replace("l", "t");
+console.log(newStr);
---
+++ Javascript/Day_26/String methods in JS/app.js
@@ -0,0 +1,40 @@
+//! Slice Method in JS
+// Return a part of the original string as a new string
+
+//Example 1
+let str = "IloveCoding";
+console.log(str.slice(1));
+console.log(str.slice(1, 5));
+console.log(str.slice(-1));
+
+//Example 2
+let name = "AdityaTile";
+console.log(name.slice(6));
+console.log(name.slice(0, 6));
+console.log(name.slice(-4));
+
+//! replace method in JS
+//Searches a value in the string & return a new string with the value replaced
+
+// Example 1
+
+let myName = "AdityaTile";
+console.log(myName.replace("Aditya", "Bablu"));
+console.log(myName.replace("Tile", "tile"));
+
+// Exapmle 2
+let clg = "MatoshriCollegeOfEngineering";
+console.log(clg.replace("Engineering", "Pharmacy"));
+console.log(clg.replace("Matoshri", "PCOE"));
+
+//! repeat method
+//Returns a string with number of copies of a string
+
+// Exapmle 1
+let friut = "Mango";
+console.log(friut.repeat(5));
+
+// Exapmle 2
+let num = "10";
+let print = num.repeat(20);
+console.log(print);
