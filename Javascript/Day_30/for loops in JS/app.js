9d7ebb46b8724d89ec222a31acbc7f95bac58999
Aditya Tile <adityatile99@gmail.com>
2024-03-21 17:46:39 +0530

    In day 30 i started learning loops

---
+++ Javascript/Day_30/for loops in JS/app.js
@@ -0,0 +1,12 @@
+//! for loop
+// used to repeat a piece of code
+
+// Example 1
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// Example 2
+for (let num = 10; num <=100; num++) {
+    console.log(num);
+}
