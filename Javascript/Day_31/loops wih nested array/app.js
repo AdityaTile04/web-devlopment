c9ee9395702fd7cd493d70402ed147f8ccf7b4e2
Aditya Tile <adityatile99@gmail.com>
2024-03-23 20:17:28 +0530

    learned nested loops with array

---
+++ Javascript/Day_31/loops wih nested array/app.js
@@ -0,0 +1,27 @@
+//! Nested loops with nested arrays
+
+// Exapmle 1
+let heroes = [
+  ["ironman", "thor", "loki"],
+  ["batman", "superman", "flash"],
+];
+
+for (i = 0; i < heroes.length; i++) {
+  console.log(heroes[i], heroes[i].length);
+  for (j = 0; j <= heroes[i].length; j++) {
+    console.log(`j= ${j}, ${heroes[i][j]}`);
+  }
+}
+
+// Example 2
+let students = [
+  ["Aditya", "Gaurav", "Kunal"],
+  ["Rushi", "Saurabh", "Shubham"],
+];
+
+for(i=0; i<students.length; i++){
+    console.log(students[i]);
+    for(j=0;j<students[i].length; j++){
+        console.log(`roll${j},${students[i][j]}`);
+    }
+}
