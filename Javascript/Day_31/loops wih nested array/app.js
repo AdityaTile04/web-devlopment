e38f52f361ccf46f90cbecfacfe0b4b0f9de0995
Aditya Tile <adityatile99@gmail.com>
2024-03-24 18:52:52 +0530

    solved nested loop with array example

--- Javascript/Day_31/loops wih nested array/app.js
+++ Javascript/Day_31/loops wih nested array/app.js
@@ -19,9 +19,22 @@ let students = [
   ["Rushi", "Saurabh", "Shubham"],
 ];
 
-for(i=0; i<students.length; i++){
-    console.log(students[i]);
-    for(j=0;j<students[i].length; j++){
-        console.log(`roll${j},${students[i][j]}`);
-    }
+for (i = 0; i < students.length; i++) {
+  console.log(students[i]);
+  for (j = 0; j < students[i].length; j++) {
+    console.log(`roll${j},${students[i][j]}`);
+  }
+}
+
+// Example 3
+let players = [
+  ["dhoni", "rahane", "jadeja"],
+  ["virat", "maxwell", "faf"],
+];
+
+for (let i =0; i< players.length; i++){
+  console.log(players[i]);
+  for(let j = 0; j<=players[i].length; j++){
+    console.log(j,players[i][j]);
+  }
 }
