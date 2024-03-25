dff81cce5d4f49ed9ed55fb5f57f6c3ae1d201c2
Aditya Tile <adityatile99@gmail.com>
2024-03-25 23:27:55 +0530

    in day 34 i learned nested objects and array of objects

---
+++ Javascript/Day_34/Nested Objects/app.js
@@ -0,0 +1,40 @@
+//! Nested Objects
+
+//! Object of Objects
+
+// storing information of multiple students
+
+// Example 1
+const classInfo = {
+  aditya: {
+    grade: "O",
+    city: "Nashik",
+  },
+  kunal: {
+    grade: "O",
+    city: "Akole",
+  },
+  saurabh: {
+    grade: "O",
+    city: "Sinner",
+  },
+  rushikesk: {
+    grade: "O",
+    city: "Niphad",
+  },
+};
+
+// Example 2
+const productPrice = {
+  laptop: {
+    price: "50,000",
+  },
+  mobile: {
+    price: "20,000",
+  },
+  ipad: {
+    price: "40,000",
+  },
+};
+
+console.log(classInfo, productPrice);
