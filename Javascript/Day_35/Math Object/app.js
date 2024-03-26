8d551bf96b75b9682388f52757a575114691e838
Aditya Tile <adityatile99@gmail.com>
2024-03-26 19:00:53 +0530

    In day 35 i learned about Math Object

---
+++ Javascript/Day_35/Math Object/app.js
@@ -0,0 +1,12 @@
+//! Math Object
+
+console.log(Math.PI);
+console.log(Math.E);
+
+//! Methods
+
+console.log(Math.abs(5));
+console.log(Math.pow(2, 4));
+console.log(Math.floor(5.555)); // Smallest
+console.log(Math.ceil(5.555)); // Largest
+console.log(Math.random());
